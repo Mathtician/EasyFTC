ea85d848f1d9188347a9a499a91b7c7aa7ba8492
Mathtician <mathtician@gmail.com>
2020-09-15 16:46:04 -0400

    Started documenting motor code

--- EasyFTCLib/src/main/java/com/lcrobotics/easyftclib/CommandCenter/driveTrain/DriveTrainException.java
+++ EasyFTCLib/src/main/java/com/lcrobotics/easyftclib/CommandCenter/driveTrain/DriveTrainException.java
@@ -1,5 +1,7 @@
 package com.lcrobotics.easyftclib.CommandCenter.driveTrain;
 
+// DriveTrain must have either 2 motors labeled LEFT and RIGHT or 4 motors with appropriate labels
+// This custom exception is thrown when that isn't the case
 public class DriveTrainException extends Exception {
     public DriveTrainException(String errorMessage){
         super(errorMessage);
--- EasyFTCLib/src/main/java/com/lcrobotics/easyftclib/CommandCenter/driveTrain/WheelPosition.java
+++ EasyFTCLib/src/main/java/com/lcrobotics/easyftclib/CommandCenter/driveTrain/WheelPosition.java
@@ -1,5 +1,7 @@
 package com.lcrobotics.easyftclib.CommandCenter.driveTrain;
 
+// First 4 values are used for motors in a 4 wheel configuration,
+// last 2 are used for a 2 wheeled robot
 public enum  WheelPosition {
     FRONT_RIGHT,
     FRONT_LEFT,
